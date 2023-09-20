11c6e8b5ba0e1007b14da53d6e7ca78989dbe6e0
FR0K3 <00174320@uca.edu.sv>
2023-09-19 22:02:08 -0600

    Add API

---
+++ payment-api/src/main/java/com/fr0k3/paymentapi/PaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.fr0k3.paymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApiApplication.class, args);
+	}
+
+}
---
+++ payment-api/src/test/java/com/fr0k3/paymentapi/PaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr0k3.paymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
